f3c44168059d53a560bffd13d8c30c5d48b308cb
jcrops <suttar@gmail.com>
2021-03-31 12:50:00 -0400

    Controller and Object functions

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{const t=("meet margaret for tea","have a nice tea with margaret and discuss politics","january 4th","low priority",{title:"meet margaret for tea",description:"have a nice tea with margaret and discuss politics",dueDate:"january 4th",priority:"low priority"});console.table(t)})();
